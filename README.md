96bb2288af61be1cd7a86a71d953cf75f8e85853
Nader Dabit <dabit3@gmail.com>
2017-02-13 18:17:03 -0600

    udpated example

---
+++ README.md
@@ -0,0 +1,16 @@
+# [React Native Tab View](https://github.com/react-native-community/react-native-tab-view) - [Ignite Plugin](https://github.com/infinitered/ignite)
+
+## To get started
+
+1. install plugin
+
+```
+ignite add tab-view
+```
+
+2. import and use [React Native Tab View](https://github.com/react-native-community/react-native-tab-view)
+
+```
+import { Button } from 'react-native-elements'
+
+<Button title="Hello Ignite" />
---
+++ index.js
@@ -0,0 +1,54 @@
+// Ignite plugin for TabView
+// ----------------------------------------------------------------------------
+
+const ReactNativeTabView = 'react-native-tab-view'
+const EXAMPLE_FILE = 'TabViewExample.js'
+const PLUGIN_PATH = __dirname
+const APP_PATH = process.cwd()
+
+const add = async function (context) {
+  const { ignite, filesystem } = context
+
+  // install a npm module and link it
+  await ignite.addModule(ReactNativeTabView)
+
+  // copy the component example file (if examples are turned on)
+  await ignite.addComponentExample(EXAMPLE_FILE, { title: 'TabView Example' })
+
+  // Example of copying templates/TabView to App/TabView
+  // if (!filesystem.exists(`${APP_PATH}/App/TabView`)) {
+  //   filesystem.copy(`${PLUGIN_PATH}/templates/TabView`, `${APP_PATH}/App/TabView`)
+  // }
+
+  // Example of patching a file
+  // ignite.patchInFile(`${APP_PATH}/App/Config/AppConfig.js`, {
+  //   insert: `import '../TabView/TabView'\n`,
+  //   before: `export default {`
+  // })
+}
+
+/**
+ * Remove yourself from the project.
+ */
+const remove = async function (context) {
+  const { ignite, filesystem, patching } = context
+
+  // remove the npm module and unlink it
+  await ignite.removeModule(ReactNativeTabView)
+
+  // remove the component example
+  await ignite.removeComponentExample(EXAMPLE_FILE)
+
+  // Example of removing App/TabView folder
+  // const removeTabView = await context.prompt.confirm(
+  //   'Do you want to remove App/TabView?'
+  // )
+  // if (removeTabView) { filesystem.remove(`${APP_PATH}/App/TabView`) }
+
+  // Example of unpatching a file
+  // patching.replaceInFile(`${APP_PATH}/App/Config/AppConfig.js`, `import '../TabView/TabView'\n`, '')
+}
+
+// Required in all Ignite plugins
+module.exports = { add, remove }
+
